77952f4de8a183caf6ba89925b8b124bbd04f5c9
Diti02 <ditibanerjee02@gmail.com>
2024-01-21 11:50:33 +0530

    update header component with user profile picture and data

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,14 @@
-import { Navbar, TextInput } from 'flowbite-react'
+import { Avatar, Dropdown, Navbar, TextInput } from 'flowbite-react'
 import { Link, useLocation } from 'react-router-dom'
 import {AiOutlineSearch} from 'react-icons/ai';
 import { Button } from 'flowbite-react';
 import {FaMoon} from 'react-icons/fa';
+import { useSelector } from 'react-redux'; 
 
 export const Header = () => {
     const path=useLocation().pathname;
+    //get whetehr current user exits or not from state
+    const {currentUser} = useSelector(state=>state.user);
   return (
     <Navbar className='border-b-2'>
     <Link to="/" className='self-center whitespace-nowrap text-xl font-semibold dark:text-white'>
@@ -24,11 +27,43 @@ export const Header = () => {
     <Button className='w-12 h-10 hidden sm:inline' color='grey' pill>
         <FaMoon></FaMoon>
     </Button>
-<Link to='/sign-in'>
+
+    {/* create a dynamic sign in button, if current user exists
+    show their avatar and dropdown
+    else show sign in */}
+    {currentUser ? (
+        <Dropdown
+        arrowIcon={false}
+        inline
+        label={<Avatar
+        alt='user'
+        img={currentUser.profilePicture}
+        rounded
+        ></Avatar>}
+        >
+
+        <Dropdown.Header>
+            <span className='block text-sm'>@{currentUser.username}</span>
+            <span className='block text-medium truncate'>{currentUser.email}</span>
+        </Dropdown.Header>
+        <Link to={'/dashboard?tab=profile'}>
+            <Dropdown.Item>Profile</Dropdown.Item>
+        </Link>
+        <Dropdown.Divider></Dropdown.Divider>
+    <Dropdown.Item>Sign out</Dropdown.Item>
+        </Dropdown>
+    ):
+    (
+    <Link to='/sign-in'>
     <Button gradientDuoTone='purpleToBlue' outline>
         Sign In 
     </Button>
-</Link>
+    </Link>
+    
+    )
+    
+    }
+
 <Navbar.Toggle></Navbar.Toggle>
 
 </div>
