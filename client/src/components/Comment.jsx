163b144efed043f643dadedfcf54a5acdbe10efb
Diti02 <ditibanerjee02@gmail.com>
2024-01-28 21:24:22 +0530

    add edit comment functionality

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -2,9 +2,11 @@ import moment from "moment";
 import { useEffect, useState } from "react"
 import {useSelector } from "react-redux";
 import {FaThumbsUp} from "react-icons/fa"
-
-export const Comment = ({comment, onLike}) => {
+import {Button, Textarea} from 'flowbite-react';
+export const Comment = ({comment, onLike, onEdit}) => {
     const [user, setUser]=useState({});
+    const [isEditing, setIsEditing]= useState(false);
+    const [editedContent, setEditedContent]=useState(comment.content);
     const {currentUser} = useSelector((state)=>state.user);
     useEffect(()=>{
         const getUser = async ()=>{
@@ -20,6 +22,32 @@ export const Comment = ({comment, onLike}) => {
         }
         getUser();
     },[comment])
+
+    const handleEdit = () =>{
+      setIsEditing(true);
+      setEditedContent(comment.content);
+
+    }
+
+    const handleSave = async () =>{
+      try{
+        const res= await fetch(`/api/comment/editComment/${comment._id}`,{
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            content: editedContent
+          })
+        });
+        if(res.ok){
+          setIsEditing(false);
+          onEdit(comment,editedContent);
+        }
+      }catch(error){
+        console.log(error.message);
+      }
+    }
   return (
     <div className="flex p-4 border-b dark:border-gray-600 text-sm">
     <div className="flex-shrink-0 mr-3">
@@ -30,7 +58,39 @@ export const Comment = ({comment, onLike}) => {
             <span className="font-bold mr-1 text-xs truncate">{user? `@${user.username}`: "anonymous user"}</span>
             <span className="text-gray-500 text-xs">{moment(comment.createdAt).fromNow()}</span>
         </div>
-        <p className="text-gray-500 pb-2">{comment.content}</p>
+        {isEditing? (
+          <>
+          <Textarea
+            className='mb-2'
+            value={editedContent}
+            onChange={(e) => setEditedContent(e.target.value)}
+          />
+          <div className="flex justify-end gap-2 text-xs">
+            <Button
+             type='button'
+             size='sm'
+             gradientDuoTone='purpleToBlue'
+              onClick={handleSave}
+            >
+              Save
+            </Button>
+            <Button
+             type='button'
+             size='sm'
+             gradientDuoTone='purpleToBlue'
+             outline
+              onClick={() => setIsEditing(false)
+              }
+            >
+              Cancel
+            </Button>
+          </div>
+          </>
+
+        ):        
+        (
+          <>
+                    <p className="text-gray-500 pb-2">{comment.content}</p>
         <div className="flex items-center pt-2 text-xs dark:border-gray-700 max-w-fit gap-2">
         <button
             type='button'
@@ -49,7 +109,21 @@ export const Comment = ({comment, onLike}) => {
                 ' ' +
                 (comment.numberOfLikes === 1 ? 'like' : 'likes')}
           </p>
+          {
+            currentUser && (currentUser._id === comment.userId || currentUser.idAdmin) && (
+              <button type='button'
+              className="text-gray-400 hover:text-blue-500"
+              onClick={handleEdit}
+              >
+                Edit
+              </button>
+            )
+          }
         </div>
+        </>
+        )
+        }
+        
     </div>
     </div>
   )
